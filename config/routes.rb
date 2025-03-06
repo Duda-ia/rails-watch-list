b49fb2cb36a203148ee918c17c4ac60bd1bb440e
maria montenegro <memtmontenegro@gmail.com>
2025-03-06 09:44:46 -0300

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  root to: "lists#index"
+
   resources :lists do
     resources :bookmarks, only: [:new, :create]
   end
-  resources :bookmarks, only: [:destroy]
-  
 
+  resources :bookmarks, only: [:destroy]
+
+  resources :movies, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
